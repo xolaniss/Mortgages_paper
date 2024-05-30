306626774f0f227fce94beb4886ffe0e3e04d2e9
Xolani Sibande <xolaniss@gmail.com>
2024-05-30 09:52:34 +0200

    updated shares charts

--- Scripts/03_sectoral_shares_distributions.R
+++ Scripts/03_sectoral_shares_distributions.R
@@ -35,6 +35,8 @@ library(urca)
 library(mFilter)
 library(car)
 
+options(scipen = 999)
+
 # Functions ---------------------------------------------------------------
 source(here("Functions", "fx_plot.R"))
 pivot_function <- function(tbl) {
@@ -90,32 +92,77 @@ density_gg <- function(
   title_tag = "household",
   year_version = FALSE,
   xlabel = "Log(FRM share)"){
+  gg_one <- 
   if(year_version == FALSE){
   data %>% 
     filter(Series == fix_flex_indicator) %>%
     filter(!Banks == "Total Banks") %>%
-    ggplot(aes(x = log(Value))) +
-    geom_density() +
+    ggplot(aes(x = Value)) +
+    geom_density(fill = "yellow", alpha = 0.5) +
+    scale_x_log10(labels = scales::label_log()) +
     theme_minimal() +
+    theme(legend.position = "none") +
+    ggtitle("Total density plot") +
     labs(
-      title = glue("Density of {title_tag} mortgage lending shares (2008-2023)"),
+      title = "Overall density",
       x = glue("{xlabel}"),
-      y = "Density"
-    )
+      y = "Density") +
+    scale_color_viridis_d()
     } else {
     data %>% 
     filter(Series == fix_flex_indicator) %>%
     filter(!Banks == "Total Banks") %>% 
-    ggplot(aes(x = log(Value))) +
-    geom_density() +
-    facet_wrap(~lubridate::year(Date)) +
+    ggplot(aes(x = Value)) +
+    geom_density(fill = "yellow", alpha = 0.5) +
+    scale_x_log10(labels = scales::label_log()) +
+    facet_wrap(~lubridate::year(Date), scale = "free", ncol = 2) +
     theme_minimal() +
+    theme(legend.position = "none") +
     labs(
-      title = glue("Density of {title_tag} mortgage lending shares (2008-2023)"),
+      title = "Overall density",
       x = glue("{xlabel}"),
-      y = "Density"
-    )
+      y = "Density") +
+      scale_color_viridis_d()
     }
+  gg_two <- 
+    if(year_version == FALSE){
+      data %>% 
+        filter(Series == fix_flex_indicator) %>%
+        filter(!Banks == "Total Banks") %>%
+        ggplot(aes(x = Value, color = Banks)) +
+        geom_density() +
+        scale_x_log10(labels = scales::label_log()) +
+        theme_minimal() +
+        theme(legend.position = "none") +
+        ggtitle("Bank level density") +
+        labs(
+          # title = glue("Density of {title_tag} mortgage lending shares (2008-2023)"),
+          x = glue("{xlabel}"),
+          y = "Density") +
+        scale_color_viridis_d()
+    } else {
+      data %>% 
+        filter(Series == fix_flex_indicator) %>%
+        filter(!Banks == "Total Banks") %>% 
+        ggplot(aes(x = Value, color = Banks)) +
+        geom_density() +
+        scale_x_log10(labels = scales::label_log()) +
+        facet_wrap(~lubridate::year(Date), scale = "free", ncol = 2) +
+        theme_minimal() +
+        theme(legend.position = "none") +
+        ggtitle("Bank level density") +
+        labs(
+          # title = glue("Density of {title_tag} mortgage lending shares (2008-2023)"),
+          x = glue("{xlabel}"),
+          y = "Density") +
+        scale_color_viridis_d()
+    }
+  
+ patchwork <-  gg_one + gg_two 
+ patchwork +
+   plot_annotation(tag_levels = "A", 
+                   title = glue("Density of {title_tag} mortgage lending shares (2008-2023)")
+                   )
 }
 
 # Import -------------------------------------------------------------
@@ -170,13 +217,13 @@ corporate_fixed_hist_by_year_gg <-
 corporate_fixed_hist_by_year_gg
 
 # Density estimates -------------------------------------------------------
-
 housing_density_gg <- 
   density_gg(
     data = housing_split_tbl,
     fix_flex_indicator = "Fixed_share",
-    title_tag = "household"
-  )
+    title_tag = "household",
+    xlabel = as.expression(quote(log_10(FRM_share)))
+  ) 
 housing_density_gg
 
 housing_density_year_gg <- 
